20abf791b8bff6b4d1fb788ccb84dd8f3ff28054
voverius <voverius@tuta.io>
2025-05-02 10:39:51 +0200

    updating bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+# Install Zsh if missing
+if ! command -v zsh >/dev/null 2>&1; then
+  echo "Installing Zsh..."
+  sudo apt update && sudo apt install -y zsh
+fi
+
+# Install Oh My Zsh if missing
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  export RUNZSH=no
+  export CHSH=no
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/changyuheng/zsh-interactive-cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-interactive-cd
+fi
+
+# Prompt to change default shell
+if [ "$SHELL" != "$(which zsh)" ]; then
+  read -p "Set Zsh as default shell? [y/N]: " confirm
+  if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    chsh -s "$(which zsh)"
+    echo "Default shell changed to Zsh."
+  else
+    echo "Skipping shell change."
+  fi
+fi
+
+echo "Zsh and Oh-My-Zsh installed."
+
+# Create main user
+if ! id "nemo" &>/dev/null; then
+  sudo useradd -m -s /bin/bash -G sudo nemo
+  echo "Created admin user 'nemo'"
+fi
+
+# Finish
+sudo -i -u nemo
+echo "Bootstrap script completed successfully."
+
+# Restart 
+read -p "Would you like to reboot now? [y/N]: " reboot_confirm
+if [[ "$reboot_confirm" =~ ^[Yy]$ ]]; then
+  sudo reboot
+fi
+
